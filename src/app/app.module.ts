d5600a11b46812c6619a1651e94e05f50ac4f58f
ViniciusCdeAlmeida <viniciusc.almeida1@gmail.com>
2017-10-11 17:47:42 -0300

    Basics Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeadersComponent } from './headers/headers.component';
+import { ArtComponent } from './art/art.component';
+import { ArtListComponent } from './art/art-list/art-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadersComponent,
+    ArtComponent,
+    ArtListComponent
   ],
   imports: [
     BrowserModule
